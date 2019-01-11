172f164d271f89083782d74aac09c11f51fe0169
Arka Ariza <arjarizami@unal.edu.co>
2019-01-10 19:19:47 -0500

    Update Language_Proyect.java

--- Language_Proyect/src/language_proyect/Language_Proyect.java
+++ Language_Proyect/src/language_proyect/Language_Proyect.java
@@ -30,7 +30,9 @@ public class Language_Proyect {
         String text = null;
         
         //Preprocessing
-        preprocessor(text);
+        ArrayList<String> lines = preprocessor(text);
+        //Lexical Analysis
+        ArrayList<LexLine> lexLines = lexicalAnalyser(lines);
     }
     
     public static void saveFile()
@@ -133,5 +135,157 @@ public class Language_Proyect {
         }
         
         return lines;
+    }
+    
+    static class LexPair{
+        public String type;
+        public String token;
+        
+        LexPair(String type, String token)
+        {
+            this.type = type;
+            this.token = token;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "<" + this.type + "," + this.token + ">";
+        }
+    }
+    
+    static class LexLine{
+        public ArrayList<LexPair> tokens;
+        
+        LexLine(ArrayList<LexPair> tokens)
+        {
+            this.tokens = tokens;
+        }
+        
+        @Override
+        public String toString()
+        {
+            String s = "";
+            for(int i=0; i<this.tokens.size(); i++)
+            {
+                s.concat(this.tokens.get(i).toString());
+            }
+            return s;
+        }
+    }
+    
+    public static ArrayList<LexLine> lexicalAnalyser(ArrayList<String> lines)
+    {
+        
+        ArrayList<LexLine> lexLines = new ArrayList();
+        for(int i=0; i<lines.size(); i++)
+        {
+            String line = lines.get(i);
+            int iword=0;
+            int fword = 0;
+            
+            ArrayList<LexPair> lexic = new ArrayList();
+            
+            while(iword < line.length())
+            {
+                
+                while(iword < line.length())
+                {
+                    char iw = line.charAt(iword);
+                    if(isOperator(iw)){
+                        lexic.add(new LexPair("operator", String.valueOf(iw)));
+                        iword++;
+                    }else if(isSymbol(iw)){
+                        lexic.add(new LexPair("symbol", String.valueOf(iw)));
+                        iword++;
+                    }else if(iw == ' '){
+                        iword++;
+                    }else{
+                        break;
+                    }
+                }
+                if(iword >= line.length())
+                {
+                    break;
+                }
+                fword = iword;
+                
+                while(fword < line.length() && !isOperator(line.charAt(fword)) && !isSymbol(line.charAt(fword)) && line.charAt(fword) != ' ')
+                {
+                    System.out.println(line.charAt(fword));
+                    fword++;
+                }
+                String word = line.substring(iword, fword);
+                String result = wordAnalyzer(word);
+                
+                if(result.equals("error"))
+                {
+                    System.out.println("ERROR!!!!!!! " + word);
+                }
+                else
+                {
+                    lexic.add(new LexPair(result, word));
+                    System.out.println("GOOD");
+                }
+                iword = fword;
+            }
+            System.out.println(lexic);
+            lexLines.add(new LexLine(lexic));
+        }
+        
+        return lexLines;
+    }
+    
+    public static String wordAnalyzer(String word)
+    {
+        String[] keywords = new String[]{"int", "while", "if"};
+        
+        for (int i = 0; i < keywords.length; i++) {
+            if(word.equals(keywords[i]))
+            {
+                return "keyword";
+            }
+        }
+        
+        if(word.matches("\\d+"))
+        {
+            return "number";
+        }
+        else if(word.matches("\\w+"))
+        {
+            return "identifier";
+        }
+        else
+        {
+            return "error";
+        }
+    }
+    
+    public static boolean isOperator(char c)
+    {
+        int[] operators = new int[]{33,37,38,42,43,45,47,60,61,62,124};
+        
+        for(int o: operators)
+        {
+            if((char)o == c)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean isSymbol(char c)
+    {
+        int[] symbols = new int[]{35,40,41,44,46,59,91,93,123,125}; 
+        
+        for(int o: symbols)
+        {
+            if((char)o == c)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
